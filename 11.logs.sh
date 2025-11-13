95902349a85e4080ff66e06f87e9c9bd8f9643d0
sreddy499 <sateeshreddy499@gmail.com>
2025-11-13 18:22:36 +0530

    added 2nd script

---
+++ 11.logs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+ID=$(id -u)
+
+echo "script name: $0"
+
+VALIDATE(){
+if [ $1-ne 0 ]
+ then 
+    echo "ERROR:: $2 ..failed "
+    exit 1
+  else
+    echo " $2 successfully installtaion done"
+ fi   
+
+}   
+
+if [ $ID -ne 0 ]
+then 
+    echo "Erorr:: Please run the script"
+    exit 1
+else 
+    echo "you are a root user"
+ fi   
+
+ yum install mysql -y   
+
+ VALIDATE $? "installing My SQL"
+
+ yum install git -y
+
+ VALIDATE $? "installing GIT"
