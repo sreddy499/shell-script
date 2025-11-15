6bf57d4052cdeb98e1008852ea1883a5e8745b68
sreddy499 <sateeshreddy499@gmail.com>
2025-11-15 15:24:18 +0530

    added 2nd script

---
+++ user.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#!/bin/bash
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+MONGODB_HOST=mongodb.daws7s.shop
+TIMESTMAP=$(date + %f-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2...$R Failed $N"
+    else
+        echo -e "$2...$G Success $N"
+    fi       
+}
+if [ $ID -ne O ]
+then 
+   echo -e  "$R error:: Please run the script with root acess $N"
+ else 
+   echo "you are root user "  
+fi
+dnf module disable nodejs -y
+VALIDATE $? "Validate Nodejs Disable"
+dnf module enable nodejs:18 -y
+VALIDATE $? "Validate Nodejs Enable"
+dnf install nodejs -y
+VALIDATE $? "Validate NodeJS install"
+useradd roboshop
+VALIDATE $? "Validate roboshop user create"
+mkdir /app
+VALIDATE $? "Validate app directory"
