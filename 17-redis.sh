295826cbfd4761659dbb2b79230770132b12cd29
sreddy499 <sateeshreddy499@gmail.com>
2025-11-15 19:58:33 +0530

    change in the script

--- 17-redis.sh
+++ 17-redis.sh
@@ -23,6 +23,6 @@ then
 fi
 dnf module enable redis:remi-6.2 -y
 dnf install redis -y
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
 systemctl enable redis
 systemctl start redis
