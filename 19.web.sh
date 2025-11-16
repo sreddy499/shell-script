b7a8e63642a2c8416d52e03e1f2c805a8854b8f2
sreddy499 <sateeshreddy499@gmail.com>
2025-11-16 08:36:45 +0530

    change in the script

--- 19.web.sh
+++ 19.web.sh
@@ -5,7 +5,7 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 MONGODB_HOST=mongodb.daws7s.shop
-TIMESTMAP=$(date + %f-%H-%M-%S)
+TIMESTMAP=$(date +%f-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 VALIDATE(){
    if [ $1 -ne 0 ]
@@ -21,12 +21,18 @@ then
  else 
    echo "you are root user "  
 fi
-dnf install nginx -y
+dnf install nginx -y &>> $LOGFILE
+VALIDATE $? "installing nginx"
 systemctl enable nginx
+VALIDATE $? "enabling nginx"
 systemctl start nginx
+VALIDATE $? "starting nginx"
 rm -rf /usr/share/nginx/html/*
+VALIDATE $? "remove default website"
 curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip
+VALIDATE $? "downloading application"
 cd /usr/share/nginx/html
+VALIDATE $? "moving to nginx"
 unzip -o /tmp/web.zip
 cp /home/centos/roboshop.config /etc/nginx/default.d/roboshop.conf 
 systemctl restart nginx 
