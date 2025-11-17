3f05468b1054ce04ee35934a004782a22da85b28
sreddy499 <sateeshreddy499@gmail.com>
2025-11-17 09:58:42 +0530

    change in the script

---
+++ 24-ifs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+file=/etc/passwd
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ ! -f $file ] # ! Denotes not exists
+then
+   echo -e "$R Source directory: $file R does not exists . $N
+fi
+
+while IFS=":" read -r username  
+do
+   echo "username: $username
+
+done < $file 
