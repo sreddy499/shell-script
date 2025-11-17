59d27f67d34dc785576b6cbf8cc9cce7c490d4a7
sreddy499 <sateeshreddy499@gmail.com>
2025-11-17 06:08:50 +0530

    change in the script

--- 23-deleteoldlogs.sh
+++ 23-deleteoldlogs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SOURCE_DIR="/tmp/logs"
-ID=$(id -u)
+
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
@@ -10,7 +10,7 @@ then
    echo -e "$R Source directory: $SOURCE_DIR does not exists . $N
 fi
 
- FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
+ FILES_TO_DELETE=$(find $SOURCE_DIR  -type f -mtime +14 -name "*.log")
  while IFS= read -r line
  do
     echo "Deleting file: $line"
